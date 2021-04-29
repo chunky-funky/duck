f37a2772aca344dfc70dc6d700e78ccb6535ce0c
chunky-funky <83296034+chunky-funky@users.noreply.github.com>
2021-04-29 11:16:11 -0400

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ duck program/frame18.bat
@@ -0,0 +1,5 @@
+@echo
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d d:\pictures\ezgif-frame-18.bmp
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+
+EXIT /B
---
+++ duck program/frame51.bat
@@ -0,0 +1,4 @@
+@echo off
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d d:\Pictures\ezgif-frame-51.png
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+EXIT /B
