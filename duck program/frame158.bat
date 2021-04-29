d413762d665761e22ffb7d88da5df0f2141666bf
chunky-funky <83296034+chunky-funky@users.noreply.github.com>
2021-04-29 11:17:09 -0400

    this took way too long but its finally all there

---
+++ duck program/frame158.bat
@@ -0,0 +1,6 @@
+@echo off
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d d:\Pictures\ezgif-frame-158.png
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+TIMEOUT /t 3
+
+EXIT /B
