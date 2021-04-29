a25de0d4297e97b612d95c8bb251f6e3de04c042
chunky-funky <83296034+chunky-funky@users.noreply.github.com>
2021-04-29 11:12:10 -0400

    but wait theres more

---
+++ duck program/duckrun.bat
@@ -0,0 +1,347 @@
+@echo
+:a
+
+start frame1.bat
+TIMEOUT /t 3
+start frame2.bat
+TIMEOUT /t 3
+start frame3.bat
+TIMEOUT /t 3
+start frame4.bat
+TIMEOUT /t 3
+start frame5.bat
+TIMEOUT /t 3
+start frame6.bat
+TIMEOUT /t 3
+start frame7.bat
+TIMEOUT /t 3
+start frame8.bat
+TIMEOUT /t 3
+start frame9.bat
+TIMEOUT /t 3
+start frame10.bat
+TIMEOUT /t 3
+start frame11.bat
+TIMEOUT /t 3
+start frame12.bat
+TIMEOUT /t 3
+start frame13.bat
+TIMEOUT /t 3
+start frame14.bat
+TIMEOUT /t 3
+start frame15.bat
+TIMEOUT /t 3
+start frame16.bat
+TIMEOUT /t 3
+start frame17.bat
+TIMEOUT /t 3
+start frame18.bat
+TIMEOUT /t 3
+start frame19.bat
+TIMEOUT /t 3
+start frame20.bat
+TIMEOUT /t 3
+start frame21.bat
+TIMEOUT /t 3
+start frame22.bat
+TIMEOUT /t 3
+start frame23.bat
+TIMEOUT /t 3
+start frame24.bat
+TIMEOUT /t 3
+start frame25.bat
+TIMEOUT /t 3
+start frame26.bat
+TIMEOUT /t 3
+start frame27.bat
+TIMEOUT /t 3
+start frame28.bat
+TIMEOUT /t 3
+start frame29.bat
+TIMEOUT /t 3
+start frame30.bat
+TIMEOUT /t 3
+start frame31.bat
+TIMEOUT /t 3
+start frame32.bat
+TIMEOUT /t 3
+start frame33.bat
+TIMEOUT /t 3
+start frame34.bat
+TIMEOUT /t 3
+start frame35.bat
+TIMEOUT /t 3
+start frame36.bat
+TIMEOUT /t 3
+start frame37.bat
+TIMEOUT /t 3
+start frame38.bat
+TIMEOUT /t 3
+start frame39.bat
+TIMEOUT /t 3
+start frame40.bat
+TIMEOUT /t 3
+start frame41.bat
+TIMEOUT /t 3
+start frame42.bat
+TIMEOUT /t 3
+start frame43.bat
+TIMEOUT /t 3
+start frame44.bat
+TIMEOUT /t 3
+start frame45.bat
+TIMEOUT /t 3
+start frame46.bat
+TIMEOUT /t 3
+start frame47.bat
+TIMEOUT /t 3
+start frame48.bat
+TIMEOUT /t 3
+start frame49.bat
+TIMEOUT /t 3
+start frame50.bat
+TIMEOUT /t 3
+start frame51.bat
+TIMEOUT /t 3
+start frame52.bat
+TIMEOUT /t 3
+start frame53.bat
+TIMEOUT /t 3
+start frame54.bat
+TIMEOUT /t 3
+start frame55.bat
+TIMEOUT /t 3
+start frame56.bat
+TIMEOUT /t 3
+start frame57.bat
+TIMEOUT /t 3
+start frame58.bat
+TIMEOUT /t 3
+start frame59.bat
+TIMEOUT /t 3
+start frame60.bat
+TIMEOUT /t 3
+start frame61.bat
+TIMEOUT /t 3
+start frame62.bat
+TIMEOUT /t 3
+start frame63.bat
+TIMEOUT /t 3
+start frame64.bat
+TIMEOUT /t 3
+start frame65.bat
+TIMEOUT /t 3
+start frame66.bat
+TIMEOUT /t 3
+start frame67.bat
+TIMEOUT /t 3
+start frame68.bat
+TIMEOUT /t 3
+start frame69.bat
+TIMEOUT /t 3
+start frame70.bat
+TIMEOUT /t 3
+start frame71.bat
+TIMEOUT /t 3
+start frame71.bat
+TIMEOUT /t 3
+start frame72.bat
+TIMEOUT /t 3
+start frame73.bat
+TIMEOUT /t 3
+start frame74.bat
+TIMEOUT /t 3
+start frame75.bat
+TIMEOUT /t 3
+start frame76.bat
+TIMEOUT /t 3
+start frame77.bat
+TIMEOUT /t 3
+start frame78.bat
+TIMEOUT /t 3
+start frame79.bat
+TIMEOUT /t 3
+start frame80.bat
+TIMEOUT /t 3
+start frame81.bat
+TIMEOUT /t 3
+start frame82.bat
+TIMEOUT /t 3
+start frame83.bat
+TIMEOUT /t 3
+start frame84.bat
+TIMEOUT /t 3
+start frame85.bat
+TIMEOUT /t 3
+start frame86.bat
+TIMEOUT /t 3
+start frame87.bat
+TIMEOUT /t 3
+start frame88.bat
+TIMEOUT /t 3
+start frame89.bat
+TIMEOUT /t 3
+start frame90.bat
+TIMEOUT /t 3
+start frame91.bat
+TIMEOUT /t 3
+start frame92.bat
+TIMEOUT /t 3
+start frame93.bat
+TIMEOUT /t 3
+start frame94.bat
+TIMEOUT /t 3
+start frame95.bat
+TIMEOUT /t 3
+start frame96.bat
+TIMEOUT /t 3
+start frame97.bat
+TIMEOUT /t 3
+start frame98.bat
+TIMEOUT /t 3
+start frame99.bat
+TIMEOUT /t 3
+start frame100.bat
+TIMEOUT /t 3
+start frame101.bat
+TIMEOUT /t 3
+start frame102.bat
+TIMEOUT /t 3
+start frame103.bat
+TIMEOUT /t 3
+start frame104.bat
+TIMEOUT /t 3
+start frame105.bat
+TIMEOUT /t 3
+start frame106.bat
+TIMEOUT /t 3
+start frame107.bat
+TIMEOUT /t 3
+start frame108.bat
+TIMEOUT /t 3
+start frame109.bat
+TIMEOUT /t 3
+start frame110.bat
+TIMEOUT /t 3
+start frame111.bat
+TIMEOUT /t 3
+start frame112.bat
+TIMEOUT /t 3
+start frame113.bat
+TIMEOUT /t 3
+start frame114.bat
+TIMEOUT /t 3
+start frame115.bat
+TIMEOUT /t 3
+start frame116.bat
+TIMEOUT /t 3
+start frame117.bat
+TIMEOUT /t 3
+start frame118.bat
+TIMEOUT /t 3
+start frame119.bat
+TIMEOUT /t 3
+start frame120.bat
+TIMEOUT /t 3
+start frame121.bat
+TIMEOUT /t 3
+start frame122.bat
+TIMEOUT /t 3
+start frame123.bat
+TIMEOUT /t 3
+start frame124.bat
+TIMEOUT /t 3
+start frame125.bat
+TIMEOUT /t 3
+start frame126.bat
+TIMEOUT /t 3
+start frame127.bat
+TIMEOUT /t 3
+start frame128.bat
+TIMEOUT /t 3
+start frame129.bat
+TIMEOUT /t 3
+start frame130.bat
+TIMEOUT /t 3
+start frame131.bat
+TIMEOUT /t 3
+start frame132.bat
+TIMEOUT /t 3
+start frame133.bat
+TIMEOUT /t 3
+start frame134.bat
+TIMEOUT /t 3
+start frame135.bat
+TIMEOUT /t 3
+start frame136.bat
+TIMEOUT /t 3
+start frame137.bat
+TIMEOUT /t 3
+start frame138.bat
+TIMEOUT /t 3
+start frame139.bat
+TIMEOUT /t 3
+start frame140.bat
+TIMEOUT /t 3
+start frame141.bat
+TIMEOUT /t 3
+start frame141.bat
+TIMEOUT /t 3
+start frame142.bat
+TIMEOUT /t 3
+start frame143.bat
+TIMEOUT /t 3
+start frame144.bat
+TIMEOUT /t 3
+start frame145.bat
+TIMEOUT /t 3
+start frame146.bat
+TIMEOUT /t 3
+start frame147.bat
+TIMEOUT /t 3
+start frame148.bat
+TIMEOUT /t 3
+start frame149.bat
+TIMEOUT /t 3
+start frame150.bat
+TIMEOUT /t 3
+start frame151.bat
+TIMEOUT /t 3
+start frame152.bat
+TIMEOUT /t 3
+start frame153.bat
+TIMEOUT /t 3
+start frame154.bat
+TIMEOUT /t 3
+start frame155.bat
+TIMEOUT /t 3
+start frame156.bat
+TIMEOUT /t 3
+start frame159.bat
+TIMEOUT /t 3
+start frame151.bat
+TIMEOUT /t 3
+start frame160.bat
+TIMEOUT /t 3
+start frame161.bat
+TIMEOUT /t 3
+start frame162.bat
+TIMEOUT /t 3
+start frame163.bat
+TIMEOUT /t 3
+start frame164.bat
+TIMEOUT /t 3
+start frame165.bat
+TIMEOUT /t 3
+start frame166.bat
+TIMEOUT /t 3
+start frame167.bat
+TIMEOUT /t 3
+start frame168.bat
+TIMEOUT /t 3
+start frame169.bat
+TIMEOUT /t 3
+start frame170.bat
+TIMEOUT /t 3
+
+goto a 
